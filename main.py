a84fd336eb05fa00ef110a29cd331dafae2051d9
Bendik <skandix@users.noreply.github.com>
2016-11-03 16:18:18 -0700

    Update main.py
    making changes so it can check if its running on windows or linux/mac and generates the respective script to the respective os... aka bat to windows and sh to mac and linux..... i'm tired

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
+from sys import platform as _os
 import requests
 import os
 
-os.path.exists("scrape.sh") and os.remove("scrape.sh")
+osEnd = ""
+xTracks = 25
+
+os.path.exists("scrape"+osEnd) and os.remove("scrape"+osEnd)
 
 cli_id = "02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea"
 artist = [ ]
-sscrape = "soundscrape -f -n 100 "
+sscrape = "soundscrape -f -n "+str(xTracks)
 
 def get_sc_uid(username):
 	requid = requests.get('http://api.soundcloud.com/resolve.json?url=http://soundcloud.com/{:s}&client_id={:s}'.format(username, cli_id)).json()
-	return requid['id']
+	return (requid['id'])
 
 def follower_scrape():
 	fScrape = requests.get('https://api.soundcloud.com/users/{:}/followings?client_id={:}&limit=1000&offset=0'.format(get_sc_uid("bendyr"), cli_id)).json()
@@ -18,17 +22,24 @@ def follower_scrape():
 	for j in fScrape['collection']:
 		artist.append(j['permalink'])
 
-	print artist
-	print len(artist)
+	print (artist)
+	print (len(artist))
 
 def soundscrape_generate():
 	
-	file = open('scrape.sh', "wb+")
+	file = open('scrape'+osEnd, "w+")
 	
 	for j in artist:
 		dmp = sscrape + j
 		file.write(dmp)
 		file.write("\n")
-		
+
+
+if _os == "linux" or _os == "linux2" or _os == "cygwin" or _os == "darwin":
+		osEnd = ".sh"
+
+elif _os == "win32":
+		osEnd = ".bat"
+
 follower_scrape()
 soundscrape_generate()

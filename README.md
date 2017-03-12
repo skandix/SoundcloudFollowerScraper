624c0352e8199ff5660759348a7c2cd8e16e9c7c
skandix <skandix@skandixGit>
2017-03-12 15:51:41 +0100

    fixed it and added args

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# ~~SoundcloudFollowerScraper ~~S
-# BROKEN ATM, NEED TO FIX... WILL FIX IN THE NEXT FEW DAY
-# -SKANDIX 26.JAN.17
+#SoundcloudFollowerScraper 
 i wanted an easy way to automatically generate a bash script that i could run to get music from the people i follow at soundcloud.
+
+´´´ bash
+python main.py --username tzer0 --api <insert_your_apikeu>
+´´´
--- main.py
+++ main.py
@@ -1,44 +1,59 @@
 #!/usr/bin/python
 from sys import platform as _os
+import argparse
 import requests
+import sys
 import os
+import re
 
+# global vars
 osEnd = ""
 xTracks = 50
-
 os.path.exists("scrape"+osEnd) and os.remove("scrape"+osEnd)
-
-cli_id = "02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea"
 artist = [ ]
 sscrape = "soundscrape -f -n " + str(xTracks)
 
+
 def get_sc_uid(username):
-	requid = requests.get('http://api.soundcloud.com/resolve.json?url=http://soundcloud.com/{:s}&client_id={:s}'.format(username, cli_id)).json()
-	return (requid['id'])
+    scrapeid = requests.get("https://soundcloud.com/"+username).text
+    return  map(lambda x: x.replace("soundcloud:users:", ""), re.findall(u'soundcloud:users:\d+', scrapeid))[0]
 
-def follower_scrape():
-	fScrape = requests.get('https://api.soundcloud.com/users/{:}/followings?client_id={:}&limit=1000&offset=0'.format(get_sc_uid("bendyr"), cli_id)).json()
-	
-	for j in fScrape['collection']:
-		artist.append(j['permalink'])
+def follower_scrape(cli_id):
+    try:
+        fScrape = requests.get('https://api.soundcloud.com/users/{:}/followings?client_id={:}&limit=1000&offset=0'.format(get_sc_uid(args.username), cli_id)).json()
+        for j in fScrape['collection']:
+            artist.append(j['permalink'])
 
-	print (artist)
-	print (len(artist))
+    except ValueError as valerr:
+        if cli_id == "" or cli_id != "":
+            print ("Error: API Key is missing/wrong")
+            sys.exit(1)
 
-def soundscrape_generate():
-	
-	file = open('scrape'+osEnd, "w+")
-	
-	for j in artist:
-		dmp = sscrape +" "+ j
-		file.write(dmp)
-		file.write("\n")
+        else:
+            print ("Error: {:} \n ").format(valerr)
+            sys.exit(1)
+
+    print (artist)
+    print (len(artist))
+
+def soundscrape_generate():	
+    with  open('scrape'+osEnd, "w+") as file:
+        for j in artist:
+            dmp = sscrape +" "+ j
+            file.write(dmp)
+	    file.write("\n")
 
 if _os == "linux" or _os == "linux2" or _os == "cygwin" or _os == "darwin":
-		osEnd = ".sh"
+    osEnd = ".sh"
 
 elif _os == "win32":
-		osEnd = ".bat"
+    osEnd = ".bat"
 
-follower_scrape()
+parser = argparse.ArgumentParser()
+parser.add_argument("--username")
+parser.add_argument("--api")
+args = parser.parse_args()
+
+follower_scrape(args.api)
 soundscrape_generate()
+
